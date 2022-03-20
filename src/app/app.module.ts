ada2b78ec15c8a138af1d09e96a2b42490b1662b
Nikolay Nikolaev <nikolay_nikolaev@epam.com>
2022-03-20 20:19:07 +0200

    Add ng bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { rootStore } from 'src/state';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot(rootStore)],
+  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot(rootStore), NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
